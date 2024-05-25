10251a5213463dcef0b8f6a037e6b96c0d7bfc15
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-26 02:28:27 +0530

    expense

--- lib/model/expence.dart
+++ lib/model/expence.dart
@@ -1,16 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+
+part 'expence.g.dart';
+
 
 // create unique id
 final uuid = const Uuid().v4();
 
+// date formatter
+final formatDate = DateFormat.yMd();
+
+// categiry icons
+final CategoryIcons = {
+  Category.food : Icons.lunch_dining,
+  Category.travel : Icons.leak_add,
+  Category.leasure : Icons.travel_explore,
+  Category.work : Icons.work,
+};
+
 // enum for catogary
 enum Category {food, travel, leasure, work}
 
+@HiveType(typeId: 1)
 class ExpenceModel {
+
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final double amount;
+
+  @HiveField(3)
   final DateTime date;
+
+  @HiveField(4)
   final Category category;
 
   // constructors
@@ -20,4 +48,9 @@ class ExpenceModel {
     required this.date,
     required this.category,
   }) : id = uuid;
+
+  // getter for format date
+  String get getFormatedDate {
+    return formatDate.format(date);
+  }
 }
